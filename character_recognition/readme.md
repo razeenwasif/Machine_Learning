16acb955ec5c7f329511f3bc3ddaf79e0ede6225
Razeen Wasif <razeen.wasif66@gmail.com>
2025-02-22 20:35:09 +1100

    building character recognition app

---
+++ character_recognition/readme.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9613b1bd3baa960260bf98fc223a885757c4bc1f9cdb7681289e5c09b74f0a5
+size 3217
---
+++ character_recognition/static/index.js
@@ -0,0 +1,120 @@
+let canvas
+let context
+let paint
+let clickX = []
+let clickY = []
+let clickDrag = []
+
+function startCanvas() {
+  canvas = document.getElementById("canvas")
+  context = canvas.getContext("2d")
+
+  context.strokeStyle = "#000000"
+  context.lineJoin = "round"
+  context.lineWidth = 8
+
+  canvas.addEventListener("touchstart", function (e) {
+    var touch = e.touches[0]
+    var mouseEvent = new MouseEvent("mousedown", {
+      clientX: touch.clientX,
+      clientY: touch.clientY,
+    })
+    canvas.dispatchEvent(mouseEvent)
+  })
+
+  canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0]
+    var mouseEvent = new MouseEvent("mousemove", {
+      clientX: touch.clientX,
+      clientY: touch.clientY,
+    })
+    canvas.dispatchEvent(mouseEvent)
+  })
+
+  canvas.addEventListener("touchend", function (e) {
+    var mouseEvent = new MouseEvent("mouseup")
+    canvas.dispatchEvent(mouseEvent)
+  })
+
+  $('#canvas').mousedown(function (e) {
+    paint = true
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false)
+    drawCanvas()
+  })
+
+  $('#canvas').mousemove(function (e) {
+    if (paint) {
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true)
+      drawCanvas()
+    }
+  })
+
+  $('#canvas').mouseup(function (e) {
+    paint = false
+    drawCanvas()
+  })
+
+  $('#canvas').mouseleave(function (e) {
+    paint = false
+  })
+}
+
+function addClick(x, y, dragging) {
+  clickX.push(x)
+  clickY.push(y)
+  clickDrag.push(dragging)
+}
+
+function clearCanvas() {
+  context.clearRect(0, 0, 200, 200)
+}
+
+function resetCanvas() {
+  clickX = []
+  clickY = []
+  clickDrag = []
+  clearCanvas()
+}
+
+function drawCanvas() {
+  clearCanvas()
+
+  for(let i=0; i<clickX.length; i++) {
+    context.beginPath()
+    if (clickDrag[i] && i) {
+      context.moveTo(clickX[i - 1], clickY[i - 1])
+    } else {
+      context.moveTo(clickX[i] - 1, clickY[i])
+    }
+    context.lineTo(clickX[i], clickY[i])
+    context.closePath()
+    context.stroke()
+  }
+}
+
+function getPixels() {
+  let rawPixels = context.getImageData(0, 0, 200, 200).data
+  let _pixels = []
+  let pixels = []
+  
+  // scaling down the image 
+  for (i=0; i < rawPixels.length; i += 4) {
+    _pixels.push(rawPixels[i + 3])
+  }
+
+  for (i=0; i < _pixels.length; i += 800) {
+    for (j=0; j < 200; j += 4) {
+      pixels.push(_pixels[i+j])
+    }
+  }
+
+  return pixels
+}
+
+
+function practiceAction() {
+  let pixels = getPixels()
+  console.log(pixels)
+  // document.getElementById("pixels").value = pixels
+  // document.getElementById("practiceEn-form").submit()
+}
